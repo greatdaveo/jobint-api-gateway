16a1ea58b18032012590e608b61fc1601ec9c44d
David Olowomeye <olowomeyedavid242@gmail.com>
2024-09-26 20:45:28 +0100

    Added Auth Service Signup Routes

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,7 @@ import http from 'http';
 import { config } from '@gateway/config';
 import { elasticSearch } from '@gateway/elasticsearch';
 import { appRoutes } from '@gateway/routes';
+import { axiosAuthInstance } from '@gateway/services/api/auth.service';
 
 const SERVER_PORT = 4000;
 const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'apiGatewayServer', 'debug');
@@ -52,6 +53,15 @@ export class GatewayServer {
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
       })
     );
+
+    // To add the bearer token to the headers when the req comes from the frontend, before the api gateway sends the req to the respective service such as auth service
+    app.use((req: Request, _res: Response, next: NextFunction) => {
+      if (req.session?.jwt) {
+        axiosAuthInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
+      }
+
+      next();
+    });
   }
 
   //   Middleware with the compress json encoded
